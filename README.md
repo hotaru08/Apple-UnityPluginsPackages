4c5558fb1b4813a29f5266023aaea7c0b916fcec
Jolyn Wong <jolyn.wong99@gmail.com>
2024-11-01 09:27:20 +0800

    Initial commit of shell script
    Created shell script UpdatePlugins.zsh
    > Call in terminal using "./UpdatePlugins.zsh/"
    
    The script does the following:
    > Clone the main Apple repos and run its build script to output the packaged plugins internally into its own "Build" folder
    > Copy the generated files to an external folder "Packages"
    > Remove the cloned repos to restore a clean state
    
    "Packages" will be where users will get the packaged plugins for use.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Apple-UnityPluginsPackages
-Contains generated packages from Apple's Unity Plugins Repository and stores them in Git for easier usage across different platforms.
+# Repository for storing Apple's Unity Plugins
+
+Stores packaged plugins from [Apple's Unity Plugins](https://github.com/apple/unityplugins.git) to allow for any users to add them to Unity Projects.\
+Refer to [Apple's Unity Plugins](https://github.com/apple/unityplugins.git) for more information or updates.
+
+## IMPORTANT NOTES!!
+
+Shell script only works on macOS. Main repository relies on macOS specific softwares (XCode) for generation.
+
+## Contents
+
+### UpdatePlugins.zsh
+
+Using a Mac, run this script in the terminal using <b>"./UnityPlugins.zsh"</b>.\
+Retrieves the latest version of [Apple's Unity Plugins](https://github.com/apple/unityplugins.git) and outputs the packaged plugins to [Packages](Packages).
+
+### Packages
+
+Folder where the packaged plugins are stored.
---
+++ UpdatePlugins.zsh
@@ -0,0 +1,48 @@
+#!/bin/zsh
+chmod +x UpdatePlugins.zsh
+
+# Apple Plugin Git Details
+apple_plugin_repo_url="https://github.com/apple/unityplugins.git"
+apple_plugin_dir="unityplugins"
+apple_plugin_build_dir="${apple_plugin_dir}/Build"
+dest="Packages"
+build_dir="Build"
+core_plugin_dir="com.apple.unityplugin.core-3.1.5"
+gamekit_plugin_dir="com.apple.unityplugin.gamekit-3.0.0"
+
+# Clone as submodule
+echo "Updating remote checkout of ${apple_plugin_repo_url} ..."
+rm -rf $apple_plugin_dir
+git clone $apple_plugin_repo_url
+
+# Change directory into generated repos
+cd $apple_plugin_dir
+
+# Apple's build script command
+python3 build.py -p Core GameKit -m macOS iOS
+
+# Change back to base directory and copy generated packages over
+cd ../
+
+if [ ! -d $dest ]; then
+    mkdir $dest
+fi
+cp -a "${apple_plugin_build_dir}" $dest
+
+# remove cloned repos, restoring back to clean state
+rm -rf $apple_plugin_dir
+
+# Change to generated package files and unzip them
+cd "${dest}/${build_dir}"
+
+if [ ! -d $core_plugin_dir ]; then
+    mkdir $core_plugin_dir
+fi
+tar --extract --file "${core_plugin_dir}.tgz" -C $core_plugin_dir
+rm -rf "${core_plugin_dir}.tgz"
+
+if [ ! -d $gamekit_plugin_dir ]; then
+    mkdir $gamekit_plugin_dir
+fi
+tar --extract --file "${gamekit_plugin_dir}.tgz" -C $gamekit_plugin_dir
+rm -rf "${gamekit_plugin_dir}.tgz"
